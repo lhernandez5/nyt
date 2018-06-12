32eb65c57816d20625edc69c63548b50210f7038
unknown <ewilsons@aol.comgit config --global user.email ewilsons@aol.comgit config --global user.email ewilsons@aol.com>
2018-06-12 14:08:08 -0400

    initial

---
+++ app.js
@@ -0,0 +1,33 @@
+
+//id's 
+// $("#search-term") holds user search text
+// $("#number-of-records") choose 1,5 or 10
+// $("#start-year")
+// $("#end-year")
+// $(#articles)  dump articles here
+
+
+
+// here is an empty search click event
+$("#search").click(function(){
+
+
+
+
+
+
+
+
+    console.log("button clicked");
+});
+
+
+
+
+
+// here is an empty clear click event
+$("#clear").click(function(){
+
+
+    console.log("button clicked");
+});
